c837c6d5f25d9b5343d19c170c3125ec58ffc4a3
renan <renan.xote@hotmail.com>
2024-01-16 19:26:02 -0300

    refactor: functions created to compress and decompress

--- compress_decompress_file.py
+++ compress_decompress_file.py
@@ -1,14 +1,25 @@
 import zlib, base64
 
 #compressing file
-data = open('file.txt', 'r').read()
-data_bytes = bytes(data, 'utf-8')
-compressed_data = base64.b64encode(zlib.compress(data_bytes))
-decoded_data = compressed_data.decode('utf-8')
-compressed_file = open('compressed.txt','w')
-compressed_file.write(decoded_data)
+def compress(inputfile,outputfile):
+    data = open(inputfile, 'r').read()
+    data_bytes = bytes(data, 'utf-8')
+    compressed_data = base64.b64encode(zlib.compress(data_bytes))
+    decoded_data = compressed_data.decode('utf-8') 
+    compressed_file = open(outputfile,'w')
+    compressed_file.write(decoded_data)
 
 
 #decompressing file
-decompressed_data = zlib.decompress(base64.b64decode(compressed_data))
+def decompress(inputfile,outputfile):
+    file_content = open(inputfile, 'r').read()
+    enconded_data = file_content.encode('utf-8')
+    decompressed_data = zlib.decompress(base64.b64decode(enconded_data + b'='))
+    decoded_data = decompressed_data.decode('utf-8')
+    file = open(outputfile,'w')
+    file.write(decoded_data)
+    file.close()
+
+
+decompress('compressed.txt','decompressed.txt')
 
