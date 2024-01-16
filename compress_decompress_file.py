3cbabf29c64d9ea74bed4d0ce1b52f33425db97d
renan <renan.xote@hotmail.com>
2024-01-16 18:56:12 -0300

    feat: compressing file code

--- compress_decompress_file.py
+++ compress_decompress_file.py
@@ -1,1 +1,11 @@
 import zlib, base64
+
+#compressing file
+data = open('file.txt', 'r').read()
+data_bytes = bytes(data, 'utf-8')
+compressed_data = base64.b64encode(zlib.compress(data_bytes))
+decoded_data = compressed_data.decode('utf-8')
+compressed_file = open('compressed.txt','w')
+compressed_file.write(decoded_data)
+
+
